9c8714e1f28b02257a7c5347846fe5fa4b9edde0
Natalia Zhornitskaya <natalia.zhorn@gmail.com>
2021-01-28 13:24:38 +0300

    feat(core): 🧚 add basic redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import {Provider} from 'react-redux';
+import store from './store';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './index.css';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
