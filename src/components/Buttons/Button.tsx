ee217067b25e9455cb9115dc6994f62382ef1da8
Natalia Zhornitskaya <natalia.zhorn@gmail.com>
2021-02-03 15:01:51 +0300

    feat(shared-ui): 🧚 button component

---
+++ src/components/Buttons/Button.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Button.styles.scss';
+
+
+type Theme = 'primary' | 'accent' | 'outline-primary' | 'outline-accent' | 'link';
+
+type ButtonProps = {
+  theme: Theme;
+  type: 'button' | 'submit';
+  disabled: boolean;
+  action: () => {};
+  children?: React.ReactNode;
+}
+
+const Button = ({ type = 'submit', action, theme = 'primary', disabled = false, children}: Partial<ButtonProps> ) => {
+  return (
+    <button
+      type={type}
+      disabled={disabled}
+      className={theme}
+      onClick={action}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button;
